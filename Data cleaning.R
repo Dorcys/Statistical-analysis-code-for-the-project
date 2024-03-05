5b60502d84318c98f9a85e990b34c189b081a426
paulagarbin <146854382+paulagarbin@users.noreply.github.com>
2024-03-05 17:05:00 +0100

    Update Data cleaning.R

--- Data cleaning.R
+++ Data cleaning.R
@@ -56,3 +56,87 @@ Trees_2$Species_index[Trees_2$`Health Status`== 0 & Trees_2$Specie =="Acer plata
 #Adding volume column
 Trees_2$Volume <- Trees_2$`S of tree` * Trees_2$Height
 ### Объем не работает из - за запятой в формуле 
+
+##########################################Paula####################################################################
+library(readr)
+library(dplyr)
+
+#Fix problem  with "," insted of "."
+locale_dec_comma <- locale(decimal_mark = ",")
+Trees <- read_csv("Trees.csv", locale = locale_dec_comma)
+View(Trees)
+
+#Translating of species names from Russian code to Latin names
+Trees$Specie[Trees$Specie == "ЛПМ"] <- "Tilia cordata"
+Trees$Specie[Trees$Specie == "Аб"] <- "Prunus armeniaca"
+Trees$Specie[Trees$Specie == "Д"] <- "Quercus robur"
+Trees$Specie[Trees$Specie == "Яс"] <- "Fraxinus excelsior"
+Trees$Specie[Trees$Specie == "ЯСО"] <- "Fraxinus excelsior"
+Trees$Specie[Trees$Specie == "ЯБЛ"] <- "Malus sylvestris"
+Trees$Specie[Trees$Specie == "Рб"] <- "Robinia pseudoacacia"
+Trees$Specie[Trees$Specie == "Р"] <- "Robinia pseudoacacia"
+Trees$Specie[Trees$Specie == "КЛО"] <- "Acer platanoides"
+Trees$Specie[Trees$Specie == "КлО"] <- "Acer platanoides"
+Trees$Specie[Trees$Specie == "ВЯЗ"] <- "Ulmus glabra"
+
+#Summary of species 
+table(Trees$Specie)
+
+#Assign the constants to calculate the volume (D = dead)
+UG <- 0.569
+TC <- 0.472
+TC_D <-  0.463
+AP <- 0.470
+AP_D <- 0.491
+FE <-  0.468
+FE_D <- 0.508
+QR <- 0.494
+QR_D <- 0.505
+MS <-  0.515
+MS_D <-  0.515
+RP <- 0.508
+RP_D <- 0.537
+PA <- 0.550
+PA_D <- 0.569
+
+#Create new column for calculations 
+Trees$Species_index <- "Species index"
+
+#Using filters for adding some indexes
+Trees$Species_index[Trees$Specie == "Ulmus glabra"] <- UG 
+Trees$Species_index[Trees$Specie == "Tilia cordata"] <- TC
+Trees$Species_index[Trees$Specie == "Prunus armeniaca"] <- PA
+Trees$Species_index[Trees$Specie == "Quercus robur"] <- QR
+Trees$Species_index[Trees$Specie == "Fraxinus excelsior"] <- FE
+Trees$Species_index[Trees$Specie == "Malus sylvestris"] <- MS
+Trees$Species_index[Trees$Specie == "Robinia pseudoacacia"] <- RP
+Trees$Species_index[Trees$Specie == "Acer platanoides"] <- AP
+
+#Using filters for adding some indexes part 2 
+Trees$Species_index[Trees$`Health Status`== 0 & Trees$Specie =="Tilia cordata"] <- TC_D 
+Trees$Species_index[Trees$`Health Status`== 0 & Trees$Specie =="Prunus armeniaca"] <- PA_D
+Trees$Species_index[Trees$`Health Status`== 0 & Trees$Specie =="Quercus robur"] < QR_D
+Trees$Species_index[Trees$`Health Status`== 0 & Trees$Specie =="Fraxinus excelsior"] <- FE_D   
+Trees$Species_index[Trees$`Health Status`== 0 & Trees$Specie =="Malus sylvestris"] <- MS_D
+Trees$Species_index[Trees$`Health Status`== 0 & Trees$Specie =="Robinia pseudoacacia"] <- RP_D 
+Trees$Species_index[Trees$`Health Status`== 0 & Trees$Specie =="Acer platanoides"] <- AP_D
+
+#Adding volume column - calculated by forestry formula 
+Trees$Volume <- Trees$`S of tree` * Trees$Height 
+
+### we need to add s of trees * trees height * indexes for each species
+
+# Renaming the "Quantity" column to "N"
+Trees <- Trees %>% rename(N = Quantity)
+
+# Renaming the "Sample" column to "Site"
+Trees <- Trees %>% rename(Site = Sample)
+
+# Renaming the "S of tree" column to "Area"
+Trees <- Trees %>% rename(Area = "S of tree")
+
+
+
+
+
+
