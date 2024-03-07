53fa85fed518666157dcff0391500853d532fc3a
paulagarbin <146854382+paulagarbin@users.noreply.github.com>
2024-03-07 14:19:26 +0100

    Update Data cleaning.R

--- Data cleaning.R
+++ Data cleaning.R
@@ -209,6 +209,38 @@ Trees$Volume[Trees$`Health Status` == 1 & Trees$Specie =="Acer platanoides"] <- 
 #Note problem with Trees$Specie need change to nubers not variables 
 
 
+###### DESCRIPTIVE STATISTICS
+# Measures of Central tendency of Volume
+mean(Trees$Volume)
+var(Trees$Volume)
+sd(Trees$Volume)
+median(Trees$Volume)
+quantile(Trees$Volume)
+summary(Trees$Volume)
+
+# Subsets of North and South locations 
+north <- subset(Trees, Site %in% c("N1", "N2", "N3"))
+south <- subset(Trees, Site %in% c("S1", "S2", "S3"))
+
+View(north)
+View(south)
+
+summary(north$Volume)
+summary(south$Volume)
+
+# Measures of Central tendency of Volume 
+# North locations
+mean_north <- mean(north$Volume)
+median_north <- median(north$Volume)
+variance_north <- var(north$Volume)
+stdev_north <- sd(north$Volume)
+
+# South locations
+mean_south <- mean(south$Volume)
+median_south <- median(south$Volume)
+variance_south <- var(south$Volume)
+stdev_south <- sd(south$Volume)
+
 
 
 
