d4e8350fcbfccb644d89ed810d596bfb85409fa8
paulagarbin <146854382+paulagarbin@users.noreply.github.com>
2024-04-05 14:13:04 +0200

    Update Descriptive statistics.R

--- Descriptive statistics.R
+++ Descriptive statistics.R
@@ -36,15 +36,39 @@ stdev_south <- sd(south$Volume)
 table(south$Specie)
 table(north$Specie)
 
-# Filter north and south subset for Acer platanoides
+###  Acer platanoides
+# Filtering north and south subset 
 AP_N <- subset(north, Specie == "Acer platanoides")
 AP_S <- subset(south, Specie == "Acer platanoides")
 View(AP_N)
 View(AP_S)
 
-# Calculate the mean volume for Acer platanoides
+# Calculate the mean volume 
 Volume_AP_N <- mean(AP_N$Volume)
 Volume_AP_S <- mean(AP_S$Volume)
 print(Volume_AP_N)
 print(Volume_AP_S)
+
+### Fraxinus excelsior
+# Filtering north and south subset 
+FE_N <- subset(north, Specie == "Fraxinus excelsior")
+FE_S <- subset(south, Specie == "Fraxinus excelsior")
+
+# Calculating the mean volume
+Volume_FE_N <- mean(FE_N$Volume)
+Volume_FE_S <- mean(FE_S$Volume)
+print(Volume_FE_N)
+print(Volume_FE_S)
+
+### Quercus robur
+# Filtering north and south subset 
+QR_N <- subset(north, Specie == "Quercus robur")
+QR_S <- subset(south, Specie == "Quercus robur")
+
+# Calculating the mean volume
+Volume_QR_N <- mean(QR_N$Volume)
+Volume_QR_S <- mean(QR_S$Volume)
+print(Volume_QR_N)
+print(Volume_QR_S)
+
 
